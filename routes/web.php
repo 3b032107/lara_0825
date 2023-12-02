c79f6479d3a2a94c57b608febf8318ec2b08dd39
廖家禾 <3b032107@gm.student.ncut.edu.tw>
2023-12-02 22:32:02 +0800

    用save方法更新資料

--- routes/web.php
+++ routes/web.php
@@ -52,8 +52,14 @@ Route::get('about',[PostController::class, 'about'])->name('posts.about');
 //$posts = Post::where('id','<',10)->orderBy('id','DESC')->get();
 //dd($posts);
 
+//$post = Post::find(1);
+//$post->update([
+//    'title' => 'updated title',
+//    'content' => 'updated content',
+//]);
+
+//用save方法需要在cmder用 artisan db:seed
 $post = Post::find(1);
-$post->update([
-    'title' => 'updated title',
-    'content' => 'updated content',
-]);
+$post->title = 'saved title';
+$post->content = 'saved content';
+$post->save();
