db27466509c7112d9d4b9b9d4026432b2679e400
廖家禾 <3b032107@gm.student.ncut.edu.tw>
2023-12-03 13:32:28 +0800

    Post透過Comments擷取所有評論

--- routes/web.php
+++ routes/web.php
@@ -79,6 +79,15 @@ Route::get('about',[PostController::class, 'about'])->name('posts.about');
 //$fourthPost = Post::find(4);
 //dd($fourthPost);
 
-$lastPost = Post::orderBy('id','DESC')->first();
-dd($lastPost);
+//$lastPost = Post::orderBy('id','DESC')->first();
+//dd($lastPost);
 
+$post = Post::find(6);
+echo '標題: ' .$post->title. '<br>';
+echo '內容: ' .$post->content. '<br>';
+echo '----------------------'.'<br>';
+$comments = $post->comments()->get();
+foreach ($comments as $comment){
+    echo '留言: '.$comment->content."<br>";
+    echo '----------------------'.'<br>';
+}
